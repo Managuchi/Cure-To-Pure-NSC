c47df4275528faeb94739221fbd7596013b6cf5b
Managuchi <itjustpersonal@outlook.com>
2020-01-12 14:39:20 +0700

    Remove debug code.

--- Cure-To-Pure-NSC/Assets/Scripts/Weapon.cs
+++ Cure-To-Pure-NSC/Assets/Scripts/Weapon.cs
@@ -15,7 +15,6 @@ public class Weapon : MonoBehaviour
         // Rotate to mouse.
         Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Debug.Log(angle);
         transform.eulerAngles = new Vector3(0f, 0f, angle);
 
         // Fire bullets.
