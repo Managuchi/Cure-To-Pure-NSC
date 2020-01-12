149d723dbfcff430ba4a3a71d09c21532f11d982
Managuchi <itjustpersonal@outlook.com>
2020-01-12 20:21:40 +0700

    Add flashing on takes damage.

--- Cure-To-Pure-NSC/Assets/Scripts/Enemy.cs
+++ Cure-To-Pure-NSC/Assets/Scripts/Enemy.cs
@@ -9,6 +9,19 @@ public class Enemy : MonoBehaviour
     public int health = 10;
     public int damage = 1;
 
+    public float flashTime = .1f;
+
+    Material defaultMat;
+    Material whiteMat;
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        defaultMat = sr.material;
+        whiteMat = Resources.Load<Material>("whiteFlash");
+    }
+
     private void Update()
     {
         if (health <= 0)
@@ -28,6 +41,13 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        sr.material = whiteMat;
+        Invoke("resetMaterial", flashTime);
+    }
+
+    private void resetMaterial()
+    {
+        sr.material = defaultMat;
     }
 
     private void Death()
